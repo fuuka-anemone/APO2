644d974313e5ddaba275f3bc16f925a48991428f
anima-anemone <78612129+anima-anemone@users.noreply.github.com>
2021-05-04 12:04:14 -0500

    catching up 2
    add as head, add at index, and a menu for easing debugging.

--- linkedlists/src/model/LinkedList.java
+++ linkedlists/src/model/LinkedList.java
@@ -2,29 +2,79 @@ package model;
 
 public class LinkedList {
 
-    private Node first;
+    private Node head; //i could also create an attribute that contains the tail of the list, this would do the adding much simpler.
+    private int size;
 
     public LinkedList(){
+        size = 0;
     }
 
-    public void setFirst(Node f){
-        first = f;
+    public void setHead(Node h){
+        head = h;
     }
 
-    public Node getFirst(){
-        return first;
+    public int getSize(){
+        return size;
     }
 
+    public Node getHead(){
+        return head;
+    }
+
+    // adds the node at the end of the list.
     public void addNode(long id){
         Node n = new Node(id);
-        if (first == null){
-            setFirst(n);
+        if (head == null){
+            setHead(n);
         } else {
-            Node current = first;
-            while (current.getNext() != null){ //searches for the last element in the list
-                current = current.getNext(); //if the current element has a next element, then sets that element as current.
+            Node tail = head;
+            while (tail.getNext() != null){ //searches for the last element (the tail) in the list.
+                tail = tail.getNext(); //if the current element has a next element, then sets the current as the tail.
             }
-            current.setNext(n); //at this point, current is the last element, then sets its next (that's null) as the element that we want to add.
+            tail.setNext(n); //at this point, current is the last element, then sets its next (that's null) as the element that we want to add.
         }
+        size++;
+    }
+
+    // adds the node as the head of the list.
+    public void addAsHead(long id){
+        Node n = new Node(id);
+        n.setNext(head);
+        setHead(n);
+        size++;
+    }
+
+    //adds the node given a certain index.
+    public boolean addNode(int index, long id){
+        boolean added = true;
+        if (index > size){ //index out of bounds.
+            added = false;
+        } else if (index == 0) { //adds it as the first element
+            addAsHead(id);
+        } else { //adds it within the list
+            Node n = new Node(id);
+            Node current = head;
+            while (index-- != 1){ // why 1 and not 0? because to add an element at a certain index, you need be at the item before the index, to add it as the next.
+                current = current.getNext();
+            }
+            n.setNext(current.getNext());
+            current.setNext(n);
+            size++;
+        }
+        return added;
+    }
+
+    //prints the nodes.
+    public void printList(){
+        Node current = head;
+        System.out.print("[");
+        for (int i = 0; i < size; i++){
+            System.out.print(current.getId());
+            if (i != size-1){
+                System.out.print(",");
+            }
+            current = current.getNext();
+        }
+        System.out.print("]");
     }
 }
--- linkedlists/src/ui/Main.java
+++ linkedlists/src/ui/Main.java
@@ -1,9 +1,53 @@
 package ui;
 import model.LinkedList;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        LinkedList ll = new LinkedList(); //size 0
+        System.out.println("| - Welcome to my linked list - | \n\n");
+        menu(s,ll);
+    }
+
+    public static void menu(Scanner sc, LinkedList l){
+        System.out.println("1. to add a new element as the head");
+        System.out.println("2. to add a new element as the tail");
+        System.out.println("3. to add a new element by index");
+        System.out.println("4. to search an element by attribute");
+        System.out.println("5. to delete an element by attribute");
+        System.out.println("6. to print the list.");
+        System.out.println("0. to exit\n");
+        int option = sc.nextInt();
+        menu(option,l,sc);
+    }
+
+    public static void menu(int o, LinkedList l, Scanner sc){
+            switch (o){
+                case 1:
+                    System.out.print("id: ");
+                    l.addAsHead(sc.nextLong());
+                    menu(sc,l);
+                case 2:
+                    System.out.print("id: ");
+                    l.addNode(sc.nextLong());
+                    menu(sc,l);
+                case 3:
+                    System.out.print("id: ");
+                    long id = sc.nextLong();
+                    System.out.print("\nindex: ");
+                    if(!l.addNode(sc.nextInt(),id)){
+                        System.out.println("error, index out of bounds");
+                    }
+                    menu(sc,l);
+                case 6:
+                    l.printList();
+                    menu(sc,l);
+                case 0:
+                    System.exit(0);
+            }
 
     }
 }
